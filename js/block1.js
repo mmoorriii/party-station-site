8f110f59e564764e119b04ff9a6ea985a88cb705
mmorrii <byryshka55@gmail.com>
2023-03-04 21:29:38 +0200

    change js file

--- js/block1.js
+++ js/block1.js
@@ -3,22 +3,22 @@ let button = document.querySelector('#block1-button');
 const inputElement = document.querySelector('#block1-input');
 
 window.addEventListener('resize', function () {
-	if (window.innerWidth >= 900) {
-		myImg.src = '../img/planet-1440.svg';
-	} else {
-		myImg.src = '../img/planet.svg';
-	}
+   if (window.innerWidth >= 900) {
+      myImg.src = './img/planet-1440.svg';
+   } else {
+      myImg.src = './img/planet.svg';
+   }
 });
 
 //---------------------------------------------------------------------------------
 // Получаем элемент, у которого нужно изменить атрибут placeholder
 // Создаем функцию, которая будет менять текст placeholder в зависимости от размера экрана
 function updatePlaceholderText() {
-	if (window.innerWidth >= 780) {
-		inputElement.placeholder = 'Введите email чтобы войти в аккаунт или создать новый';
-	} else {
-		inputElement.placeholder = 'Введите email ';
-	}
+   if (window.innerWidth >= 780) {
+      inputElement.placeholder = 'Введите email чтобы войти в аккаунт или создать новый';
+   } else {
+      inputElement.placeholder = 'Введите email ';
+   }
 }
 
 // Вызываем функцию, чтобы установить начальный текст placeholder
@@ -28,14 +28,14 @@ updatePlaceholderText();
 window.addEventListener('resize', updatePlaceholderText);
 
 button.addEventListener('click', (event) => {
-	if (inputElement.value.trim() === '' || !inputElement.value.includes('@')) {
-		inputElement.classList.add('red-border');
-		button.classList.add('red-border');
-		inputElement.placeholder = 'ошибка';
-		event.preventDefault();
-	} else {
-		inputElement.classList.remove('red-border');
-		button.classList.remove('red-border');
-		updatePlaceholderText();
-	}
+   if (inputElement.value.trim() === '' || !inputElement.value.includes('@')) {
+      inputElement.classList.add('red-border');
+      button.classList.add('red-border');
+      inputElement.placeholder = 'ошибка';
+      event.preventDefault();
+   } else {
+      inputElement.classList.remove('red-border');
+      button.classList.remove('red-border');
+      updatePlaceholderText();
+   }
 })
